ec384a2ff3c8c04dff51bfc81813567f3b2dc875
Rhezna <99413088+Rhezna@users.noreply.github.com>
2026-04-03 01:49:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server running...");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
